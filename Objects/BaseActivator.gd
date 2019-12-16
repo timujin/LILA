1cdb5dff54a991d76aabe3d7f012216fdc962320
timujin <newtimujin@gmail.com>
2019-12-16 15:58:59 +0300

    Initial commit

---
+++ Objects/BaseActivator.gd
@@ -0,0 +1,18 @@
+extends Node
+
+class_name Activator
+
+signal activated
+
+onready var clickarea = get_parent().get_node("ClickArea")
+
+func activate():
+	if clickarea != null:
+		print("Activated %s!" % clickarea.ActivatorName)
+	else:
+		printerr("Activation area not set!")
+	emit_signal("activated")
+	
+	if has_method("on_activate"):
+		self.call("on_activate")
+		
