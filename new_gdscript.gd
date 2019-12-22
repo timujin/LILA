62ae940e48e722ae73f8f94e0ed536dd0bcf860b
timujin <newtimujin@gmail.com>
2019-12-22 15:57:56 +0300

    Jinky janky

--- new_gdscript.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-export(int) var health
-export(Resource) var sub_resource
-export(Array, String) var strings
-
-func _init(p_health = 0, p_sub_resource = null, p_strings = []):
-    health = p_health
-    sub_resource = p_sub_resource
-    strings = p_strings
