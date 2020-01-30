cc97e90e21bd2a2177478bab0988d1888acbdc81
timujin <newtimujin@gmail.com>
2020-01-30 13:17:11 +0300

    Looting window

--- Autoload/Inventory/ItemRender.gd
+++ Autoload/Inventory/ItemRender.gd
@@ -3,21 +3,26 @@ class_name ItemRender
 var resource:InventoryItem 
 var icon    :Texture
 var script  :ItemScript
+var iid     :String
 
 const defaultIcon = preload("res://Autoload/Inventory/DefaultIcon.png")
 const defaultScript = preload("res://Autoload/Inventory/ItemScript.gd")
 const errorItem = "MissingNo"
 
 func _init(id:String):
+	print("Loading..."+id)
 	resource = load("res://Autoload/Inventory/Items/%s/%s.tres" % [id,id])
 	icon = load("res://Autoload/Inventory/Items/%s/%s.png"  % [id,id])
 	script = load("res://Autoload/Inventory/Items/%s/%s.gd"   % [id,id])
 	if resource == null:
-		resource = load("res://Autoload/Inventory/Items/%s/%s.tres" % [errorItem,errorItem]).duplicate()
+		resource = load("res://Autoload/Inventory/Items/%s/%s.tres" % [errorItem,errorItem]).duplicate(true)
+		resource.name = id
 		resource.long_description = "Item not found: " + id
 		icon = load("res://Autoload/Inventory/Items/%s/%s.png"  % [errorItem,errorItem])
 		script = load("res://Autoload/Inventory/Items/%s/%s.gd"   % [errorItem,errorItem])
-		id = errorItem
+		#id = errorItem
+		print("Loading:")
+		print(resource.long_description)
 	if icon == null:
 		icon = defaultIcon
 	if script == null:
