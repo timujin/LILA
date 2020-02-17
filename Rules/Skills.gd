e01fee3a8ff04840e4059d02365802b2b73a82f5
timujin <newtimujin@gmail.com>
2020-02-17 11:58:46 +0300

    Skill score screen

--- Rules/Skills.gd
+++ Rules/Skills.gd
@@ -1,78 +1,93 @@
 extends GDScript
 
-var skill_names = [
-	"Acuity",
-	"Alchemy",
-	"Attainment",
-	"Audacity",
-	"Awareness",
-	"Brutality",
-	"Catma",
-	"Charisma",
-	"Curiosity",
-	"Determination",
-	"Dogma",
-	"Exploration",
-	"Imagination",
-	"Insight",
-	"Life",
-	"Lore",
-	"Love",
-	"Luck",
-	"Rationality",
-	"Resourcefulness",
-	"Time",
-	"Trickery",
-	"Vitality"
-]
+class_name SkillRules
 
-func skill_descr(skill:String):
+static func skill_names():
+	return [
+		"Acuity",
+		"Alchemy",
+		"Attainment",
+		"Audacity",
+		"Awareness",
+		"Brutality",
+		"Catma",
+		"Charisma",
+		"Curiosity",
+		"Determination",
+		"Dogma",
+		"Exploration",
+		"Imagination",
+		"Insight",
+		"Life",
+		"Lore",
+		"Love",
+		"Luck",
+		"Rationality",
+		"Resourcefulness",
+		"Time",
+		"Trickery",
+		"Vitality"
+	]
+
+static func skill_description(skill:String)->String:
 	match skill:
 		"Acuity":
-			return ""
+			return  "Precision and power. Clarity of purpose. "+\
+					"Acuity is your ability to keep the eye on the ball. "+\
+					"Your mind is a blade, and a sharper blade cuts deeper. "+\
+					"Life is an exact science, so be an exact person."
 		"Alchemy":
-			return ""
+			return "Nothing is permanent. Everything decays. Changes. "+\
+				   "Stops being one thing and starts being something else. "+\
+				   "Even the universe itself. Even you. This is essential to understand and accept. "+\
+				   "And then — master. Alchemy is your ability to notice, endure, and produce change."
 		"Attainment":
-			return ""
+			return "[DESCRIPTION OF ATTAINMENT NIY]"
 		"Audacity":
-			return ""
+			return "Asking for permission is for lesser beings. You only ask for forgiveness. "+\
+				   "[FULL DESCRIPTION OF AUDACITY NIY]"
 		"Awareness":
-			return ""
+			return "The world is there, whether you want it or not. Learn to be present in it. "+\
+				   "Learn to see, to notice. Knowing where and when things are."
 		"Brutality":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Catma":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Charisma":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Curiosity":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Determination":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Dogma":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Exploration":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Imagination":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Insight":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Life":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Lore":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Love":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Luck":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Rationality":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Resourcefulness":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Time":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Trickery":
-			return ""
+			return "[DESCRIPTION NIY]"
 		"Vitality":
-			return ""
-		_:
-			return ""
+			return "[DESCRIPTION NIY]"
+		var x:
+			printerr("ERROR: Skill not found: %s" % x)
+			return "ERROR: Skill not found: %s" % x
+			
+static func skill_icon(skill:String)->Texture:
+	return preload("res://Assets/Placeholders/adeptmastersorc.png")
