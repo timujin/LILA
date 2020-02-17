3c0b013ccee8db5a96723889a5a4dc979ce03fae
timujin <newtimujin@gmail.com>
2020-02-17 09:03:34 +0300

    ...

---
+++ Rules/Skills.gd
@@ -0,0 +1,78 @@
+extends GDScript
+
+var skill_names = [
+	"Acuity",
+	"Alchemy",
+	"Attainment",
+	"Audacity",
+	"Awareness",
+	"Brutality",
+	"Catma",
+	"Charisma",
+	"Curiosity",
+	"Determination",
+	"Dogma",
+	"Exploration",
+	"Imagination",
+	"Insight",
+	"Life",
+	"Lore",
+	"Love",
+	"Luck",
+	"Rationality",
+	"Resourcefulness",
+	"Time",
+	"Trickery",
+	"Vitality"
+]
+
+func skill_descr(skill:String):
+	match skill:
+		"Acuity":
+			return ""
+		"Alchemy":
+			return ""
+		"Attainment":
+			return ""
+		"Audacity":
+			return ""
+		"Awareness":
+			return ""
+		"Brutality":
+			return ""
+		"Catma":
+			return ""
+		"Charisma":
+			return ""
+		"Curiosity":
+			return ""
+		"Determination":
+			return ""
+		"Dogma":
+			return ""
+		"Exploration":
+			return ""
+		"Imagination":
+			return ""
+		"Insight":
+			return ""
+		"Life":
+			return ""
+		"Lore":
+			return ""
+		"Love":
+			return ""
+		"Luck":
+			return ""
+		"Rationality":
+			return ""
+		"Resourcefulness":
+			return ""
+		"Time":
+			return ""
+		"Trickery":
+			return ""
+		"Vitality":
+			return ""
+		_:
+			return ""
