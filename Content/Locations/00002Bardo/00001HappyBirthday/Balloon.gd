8090444a862fd8070fb3a0de2e69b10b874ef76b
timujin <newtimujin@gmail.com>
2020-03-14 17:19:35 +0300

    Popping behaviour

---
+++ Content/Locations/00002Bardo/00001HappyBirthday/Balloon.gd
@@ -0,0 +1,11 @@
+extends Activator
+
+func on_activate():
+	get_parent().queue_free()
+	var popped = Sprite.new()
+	get_parent().get_parent().add_child(popped)
+	popped.global_position = get_parent().get_node("Sprite").global_position
+	popped.texture = load("res://Content/Locations/00002Bardo/00001HappyBirthday/Graphics/Balloons/Pop.png")
+	popped.scale = Vector2(0.5, 0.5)
+	var timer = get_tree().create_timer(0.15)
+	timer.connect("timeout",popped,"queue_free") 
