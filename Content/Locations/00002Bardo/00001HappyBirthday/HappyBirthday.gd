46c6cb350992ad78f75ea30d91f01282f906b5e8
timujin <newtimujin@gmail.com>
2020-03-14 16:59:59 +0300

    HAPPY BIRTHDAY

---
+++ Content/Locations/00002Bardo/00001HappyBirthday/HappyBirthday.gd
@@ -0,0 +1,13 @@
+extends BaseRoom
+
+func get_room_name()->String:
+	return "Happy birthday room"
+	
+func get_hud_enabled()->bool:
+	return true
+	
+static func get_default_parameters()->Dictionary:
+	return {}
+	
+func render()->void:
+	pass
