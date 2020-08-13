f8a8c0b2d079716cd33114625b348a842a0a817a
timujin <newtimujin@gmail.com>
2020-08-13 17:05:36 +0300

    Ironing out kinks in the serialization model

--- Systems/Rooms/BaseRoom.gd
+++ Systems/Rooms/BaseRoom.gd
@@ -10,26 +10,29 @@ export var roomID:String   = "[ID NOT SET]"
 func _ready():
 	RoomSwitcher.room = self
 	call_deferred("move_to_back")
-	call_deferred("unpickle")
-	#call_deferred("deserialize")
-	call_deferred("emit_signal", "room_loaded")
+	#call_deferred("unpickle")
+	call_deferred("deserialize")
 	
 func move_to_back():
 	get_parent().move_child(self,0)
 
 
 func deserialize():
+	print("!!!@!")
 	#Character.pickles.pickle("", "", {})
 	var objects = get_tree().get_nodes_in_group("serializable_room")
 	for object in objects:
-		var data = Character.objects.deserialize($"/root/Room".roomID, get_path(), {})
+		var data = Character.objects.deserialize($"/root/Room".roomID, object.get_path(), {})
 		if data.size() > 0:
 			object.deserialize(data)
+	RoomSwitcher.room_deserialized = true
+	call_deferred("emit_signal", "room_loaded")
 
 func serialize():
+	print ("All seriazlied")
 	var objects = get_tree().get_nodes_in_group("serializable_room")
 	for object in objects:
-		Character.objects.serialize($"/root/Room".roomID, get_path(), object.serialize())
+		Character.objects.serialize($"/root/Room".roomID, object.get_path(), object.serialize())
 	
 
 # pickles are obsolete; still present for history	
